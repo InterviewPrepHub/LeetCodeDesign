48723cf42a7f7805b02a5a6deef5ccbd391c901f
Divya Paliwal <div.pali@gmail.com>
2024-03-02 12:54:29 +0530

    first commit

---
+++ src/main/java/com/example/LeetCodeDesign/LeetCodeDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.LeetCodeDesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetCodeDesignApplication.class, args);
+	}
+
+}
